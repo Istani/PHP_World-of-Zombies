601b3d8a895aa34b2ddccee27ee85084f74f89d3
Sascha Kaufmann <Sascha.u.Kaufmann@googlemail.com>
2013-05-01 17:16:53 +0200

    Lokale Mysql Einstellungen

--- _mysql.php
+++ _mysql.php
@@ -1,8 +1,8 @@
 <?php
 	$mysql['host']  =   "localhost";
-	$mysql['user']  =   "db11106940-bg";
-	$mysql['pw']    =   "zodwa1000";
-	$mysql['db']    =   "db11106940-game";
+	$mysql['user']  =   "root";
+	$mysql['pw']    =   "";
+	$mysql['db']    =   "zodgame";
 	
 	mysql_connect($mysql['host'], $mysql['user'], $mysql['pw']) or die ("Es konnte keine Verbindung zum Datenbankserver aufgebaut werden!");
 	mysql_select_db($mysql['db']) or die ("Die Datenbank konnte nicht geöffnet werden!");
