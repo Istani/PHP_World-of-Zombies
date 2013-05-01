fd0984807296dc07f5c8b97c429c99e9ea427169
Sascha Kaufmann <Sascha.u.Kaufmann@googlemail.com>
2013-05-01 19:50:52 +0200

    Schon einmal auf Version 0.08 gestellt, damit das nicht vergessen wird

--- _settings.php
+++ _settings.php
@@ -4,7 +4,7 @@
 	ini_set('display_errors', 1);
     $bg["title"]      =       "World of Zombies";
     $bg["author"]     =       "ZoDIkarus & ZoD-Istani";
-    $bg["version"]    =       "Version 0.07";
+    $bg["version"]    =       "Version 0.08";
 	
 	$bg['sprache']	  = "deutsch";
 ?>
