bc72ab1e2ec3a3f643f611f62d78ed3623abb25e
Alex Amling <RebornIkarus@gmail.com>
2013-05-11 21:22:09 +0200

    Changes Information,Inventory,Skills

--- site/inventory.php
+++ site/inventory.php
@@ -1,3 +1,13 @@
+<div id="tabs">
+<ul>
+<li><a href="#tabs-1">Information</a></li>
+<li><a href="#tabs-2">Inventar</a></li>
+<li><a href="#tabs-3">Skills</a></li>
+</ul>
+<div id="tabs-1">
+<p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+</div>
+<div id="tabs-2">
 <?php
 	//items auslesen
 	$sql_query = "SELECT * FROM `char` WHERE `userID` = '" . $_SESSION['userID'] . "'";
@@ -13,7 +23,7 @@
 		$char_handschuhe	=		$dsatz['handschuhe'];
 		$char_schuhe		=		$dsatz['schuhe'];
 		$char_fahrzeug		=		$dsatz['fahrzeug'];
-		
+
 		$max_wert_ausdauer = $dsatz['gesundheit'];
 
     $queryString = strstr($_SERVER['REQUEST_URI'], '?');
@@ -162,3 +172,17 @@
         return false;
     };
 </script>
+
+</div>
+<div id="tabs-3">
+<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+</div>
+</div>
+
+<script>
+$(function() {
+$( "#tabs" ).tabs();
+});
+</script>
+
