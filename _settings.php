3c1c226e42297b85320dd6934d499b6bc4e1a6e4
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2013-06-05 21:20:15 +0200

    Fix: MySQL Injection ?

--- _settings.php
+++ _settings.php
@@ -1,19 +1,35 @@
 <?php 
-	session_start();
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
+    session_start();
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    
     $bg["title"]      =       "World of Zombies";
     $bg["author"]     =       "ZoDIkarus & ZoD-Istani";
     $bg["version"]    =       "Version 0.13";
 	
-	$bg['sprache']	  = "deutsch";
+    $bg['sprache']	  = "deutsch";
 	
-	$bg['vit_ausdauer_modifier']	  = "20";
-	$bg['str_schaden_modifier']	  = "0.2";
-	$bg['luck_drop_modifier']	  = "15";
-	$bg['next_drop_modifier']	  = "0.60";
+    $bg['vit_ausdauer_modifier']	  = "20";
+    $bg['str_schaden_modifier']	  = "0.2";
+    $bg['luck_drop_modifier']	  = "15";
+    $bg['next_drop_modifier']	  = "0.60";
 	
-	// Rechnungen:
-	// https://docs.google.com/spreadsheet/pub?key=0AityTNNaT9hQdGZ1Vnc4RXh1OEVVM2pSdHhNQ2hfY3c&output=html
+    // Rechnungen:
+    // https://docs.google.com/spreadsheet/pub?key=0AityTNNaT9hQdGZ1Vnc4RXh1OEVVM2pSdHhNQ2hfY3c&output=html
 	
+    // Der Sicherheit wegen:
+    foreach ($_COOKIE as $key => $value) { 
+        if(get_magic_quotes_gpc()) $_COOKIE[$key]=stripslashes($value); 
+        $_COOKIE[$key] = mysql_real_escape_string($value); 
+    } 
+    foreach ($_POST as $key => $value) { 
+        if(get_magic_quotes_gpc()) $_POST[$key]=stripslashes($value); 
+        $_POST[$key] = mysql_real_escape_string($value); 
+    } 
+    foreach ($_GET as $key => $value) { 
+        if(get_magic_quotes_gpc()) $_GET[$key]=stripslashes($value); 
+        $_GET[$key] = mysql_real_escape_string($value); 
+    } 
+    
+    
 ?>
