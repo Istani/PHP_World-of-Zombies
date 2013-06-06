f6740157d4a09955802ff614d607c445d756877a
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2013-06-06 20:03:27 +0200

    Add: Char Stats Übersicht

--- site/charakter_datenauslesen.php
+++ site/charakter_datenauslesen.php
@@ -1,28 +1,29 @@
 <?php
-	//Einfach alle felder auslesen :-P
-	$sql['kd_stamm'] = "SELECT * FROM `char` WHERE `userID` = '" . $_SESSION['userID'] . "'";
-	$query['kd_stamm']=mysql_query($sql['kd_stamm']);
-	while ($row['kd_stamm']=mysql_fetch_assoc($query['kd_stamm'])) {
-		foreach ($row['kd_stamm'] as $key => $value) {
-			$varb_text="char_".$key;
-			$$varb_text=$value; // Variable Variablenamen!
-		}
-		$max_wert_ausdauer = $row['kd_stamm']['gesundheit']*$bg['vit_ausdauer_modifier'];
-	}
-	
-	$sql_exp = "SELECT * FROM `char_exp` WHERE `level` = '" . $_SESSION['lvl'] . "'";
-	$result_exp = mysql_query($sql_exp);
-	$dsatz_exp = mysql_fetch_assoc($result_exp);
 
-	$sql_time = "SELECT * FROM `login` WHERE `userID` = '" . $_SESSION['userID'] . "'";
-	$result_time = mysql_query($sql_time);
-	$dsatz_time = mysql_fetch_assoc($result_time);
+//Einfach alle felder auslesen :-P
+$sql['kd_stamm'] = "SELECT * FROM `char` WHERE `userID` = '" . $_SESSION['userID'] . "'";
+$query['kd_stamm'] = mysql_query($sql['kd_stamm']);
+while ($row['kd_stamm'] = mysql_fetch_assoc($query['kd_stamm'])) {
+    foreach ($row['kd_stamm'] as $key => $value) {
+        $varb_text = "char_" . $key;
+        $$varb_text = $value; // Variable Variablenamen!
+    }
+    $max_wert_ausdauer = $row['kd_stamm']['gesundheit'] * $bg['vit_ausdauer_modifier'];
+}
+$char_stats = get_player_status($_SESSION['userID']);
 
-	$tage = (int)($dsatz_time['onlineTimer']/60/60/24);
-	$stunden = (int)($dsatz_time['onlineTimer']/60/60-$tage*24);
-	$minuten = (int)($dsatz_time['onlineTimer']/60-($tage*24+$stunden)*60);
-	
-	$stunden=sprintf ( "%02d",$stunden);
-	$minuten=sprintf ( "%02d",$minuten);
+$sql_exp = "SELECT * FROM `char_exp` WHERE `level` = '" . $_SESSION['lvl'] . "'";
+$result_exp = mysql_query($sql_exp);
+$dsatz_exp = mysql_fetch_assoc($result_exp);
 
+$sql_time = "SELECT * FROM `login` WHERE `userID` = '" . $_SESSION['userID'] . "'";
+$result_time = mysql_query($sql_time);
+$dsatz_time = mysql_fetch_assoc($result_time);
+
+$tage = (int) ($dsatz_time['onlineTimer'] / 60 / 60 / 24);
+$stunden = (int) ($dsatz_time['onlineTimer'] / 60 / 60 - $tage * 24);
+$minuten = (int) ($dsatz_time['onlineTimer'] / 60 - ($tage * 24 + $stunden) * 60);
+
+$stunden = sprintf("%02d", $stunden);
+$minuten = sprintf("%02d", $minuten);
 ?>
--- site/charakter_informationen.php
+++ site/charakter_informationen.php
@@ -1,100 +1,139 @@
 <h1>Status</h1>
 <hr />
 <table width="100%">
-	<tr>
-		<td style="width: 25%"><b><?php echo $_SESSION['loginName']; ?></b></td>
-		<td style="width: 25%"></td>
-		<td style="width: 25%"></td>
-		<td style="width: 25%"><b>Spielzeit:</b></td>
-	</tr>
-	<tr>
-		<td>Level: <?php echo $dsatz['level']; ?></td>
-		<td>Klasse: <?php echo text_ausgabe("char_klasse", $dsatz['klasse'], $bg['sprache']); ?></td>
-		<td>&nbsp;</td>
-		<td><?php echo $tage;?> Tage <?php echo $stunden;?> Std. <?php echo $minuten;?> Min.</td>
-	</tr>
-	<tr>
-		<td colspan="4">
-			<hr />
-			&nbsp;<br>
-			&nbsp;<br>
-		</td>
-	</tr>
-	<tr>
-		<td>Erfahrung:</td>
-		<td></td>
-		<td></td>
-		<td class="text_rechts"><?php echo $dsatz["exp"] . "/" . $dsatz_exp["exp"]; ?></td>
-	</tr>
-	<tr>
-		<td colspan="4">
-			<?php
-			echo '<div id="Exp_bar_status" class="Exp_bg"></div>';
-			?>
-			<script>
-			var $jq = jQuery.noConflict();
-				$jq(function() {
-				$jq( "#Exp_bar_status" ).progressbar({
-					value: <?php echo $dsatz["exp"]; ?>,
-					max: <?php echo $dsatz_exp["exp"]; ?>
-				});
-			});
-			</script>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="4">
-			&nbsp;<br>
-			&nbsp;
-		</td>
-	</tr>
-	<tr>
-		<td>Nahrung:</td>
-		<td class="text_rechts"><?php echo $dsatz['nahrung']; ?>/<?php echo get_wert_plus_bonus($_SESSION['userID'], "nahrung", $max_wert_ausdauer); ?></td>
-		<td>Wasser:</td>
-		<td class="text_rechts"><?php echo $dsatz['wasser']; ?>/<?php echo get_wert_plus_bonus($_SESSION['userID'], "wasser", $max_wert_ausdauer); ?></td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<?php
-			echo '<div id="Nahrung_bar_status" class="Nahrung_bg"></div>';
-			?>
-			<script>
-			var $jq = jQuery.noConflict();
-				$jq(function() {
-				$jq( "#Nahrung_bar_status" ).progressbar({
-					value: <?php echo $char_nahrung; ?>,
-					max: <?php echo get_wert_plus_bonus($_SESSION['userID'], "nahrung", $max_wert_ausdauer); ?>
-				});
-			});
-			</script>
-		</td>
-		<td colspan="2">
-			<?php
-			echo '<div id="Wasser_bar_status" class="Wasser_bg"></div>';
-			?>
-			<script>
-			var $jq = jQuery.noConflict();
-				$jq(function() {
-				$jq( "#Wasser_bar_status" ).progressbar({
-					value: <?php echo $char_wasser; ?>,
-					max: <?php echo get_wert_plus_bonus($_SESSION['userID'], "wasser", $max_wert_ausdauer); ?>
-				});
-			});
-			</script>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="4">
-			&nbsp;<br>
-			&nbsp;<br>
-			&nbsp;
-		</td>
-	</tr>
-	<tr>
-		<td>Gegenst&auml;nde hergestellt:</td>
-		<td class="text_rechts"><?php echo $dsatz["Items_Crafting"]; ?></td>
-		<td>Rohstoffe abgebaut:</td>
-		<td class="text_rechts"><?php echo $dsatz["Items_Abbau"]; ?></td>
-	</tr>
+    <tr>
+        <td style="width: 25%"><b><?php echo $_SESSION['loginName']; ?></b></td>
+        <td style="width: 25%"></td>
+        <td style="width: 25%"></td>
+        <td style="width: 25%"><b>Spielzeit:</b></td>
+    </tr>
+    <tr>
+        <td>Level: <?php echo $dsatz['level']; ?></td>
+        <td>Klasse: <?php echo text_ausgabe("char_klasse", $dsatz['klasse'], $bg['sprache']); ?></td>
+        <td>&nbsp;</td>
+        <td><?php echo $tage; ?> Tage <?php echo $stunden; ?> Std. <?php echo $minuten; ?> Min.</td>
+    </tr>
+    <tr>
+        <td colspan="4">
+            <hr />
+            &nbsp;<br>
+            &nbsp;<br>
+        </td>
+    </tr>
+    <tr>
+        <td>Erfahrung:</td>
+        <td></td>
+        <td></td>
+        <td class="text_rechts"><?php echo $dsatz["exp"] . "/" . $dsatz_exp["exp"]; ?></td>
+    </tr>
+    <tr>
+        <td colspan="4">
+            <?php
+            echo '<div id="Exp_bar_status" class="Exp_bg"></div>';
+            ?>
+            <script>
+                var $jq = jQuery.noConflict();
+                $jq(function() {
+                    $jq("#Exp_bar_status").progressbar({
+                        value: <?php echo $dsatz["exp"]; ?>,
+                        max: <?php echo $dsatz_exp["exp"]; ?>
+                    });
+                });
+            </script>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="4">
+            &nbsp;<br>
+            &nbsp;
+        </td>
+    </tr>
+    <tr>
+        <td>Nahrung:</td>
+        <td class="text_rechts"><?php echo $dsatz['nahrung']; ?>/<?php echo get_wert_plus_bonus($_SESSION['userID'], "nahrung", $max_wert_ausdauer); ?></td>
+        <td>Wasser:</td>
+        <td class="text_rechts"><?php echo $dsatz['wasser']; ?>/<?php echo get_wert_plus_bonus($_SESSION['userID'], "wasser", $max_wert_ausdauer); ?></td>
+    </tr>
+    <tr>
+        <td colspan="2">
+            <?php
+            echo '<div id="Nahrung_bar_status" class="Nahrung_bg"></div>';
+            ?>
+            <script>
+                var $jq = jQuery.noConflict();
+                $jq(function() {
+                    $jq("#Nahrung_bar_status").progressbar({
+                        value: <?php echo $char_nahrung; ?>,
+                        max: <?php echo get_wert_plus_bonus($_SESSION['userID'], "nahrung", $max_wert_ausdauer); ?>
+                    });
+                });
+            </script>
+        </td>
+        <td colspan="2">
+            <?php
+            echo '<div id="Wasser_bar_status" class="Wasser_bg"></div>';
+            ?>
+            <script>
+                var $jq = jQuery.noConflict();
+                $jq(function() {
+                    $jq("#Wasser_bar_status").progressbar({
+                        value: <?php echo $char_wasser; ?>,
+                        max: <?php echo get_wert_plus_bonus($_SESSION['userID'], "wasser", $max_wert_ausdauer); ?>
+                    });
+                });
+            </script>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="4">
+            &nbsp;<br>
+            &nbsp;<br>
+            &nbsp;
+        </td>
+    </tr>
+    <tr>
+        <td><?php echo text_ausgabe("char_gesundheit", 0, $bg['sprache']); ?></td>
+        <td><?php echo $char_gesundheit . " (" . $char_stats['vit'] . ")"; ?></td>
+        <td><?php echo text_ausgabe("char_wissen", 0, $bg['sprache']); ?></td>
+        <td><?php echo $char_wissen . " (" . $char_stats['int'] . ")"; ?></td>
+    </tr>
+    <tr>
+        <td><?php echo text_ausgabe("char_power", 0, $bg['sprache']); ?></td>
+        <td><?php echo $char_power . " (" . $char_stats['str'] . ")"; ?></td>
+        <td><?php echo text_ausgabe("char_geschwindigkeit", 0, $bg['sprache']); ?></td>
+        <td><?php echo $char_geschwindigkeit . " (" . $char_stats['agi'] . ")"; ?></td>
+    </tr>
+    <tr>
+        <td><?php echo text_ausgabe("char_treffchance", 0, $bg['sprache']); ?></td>
+        <td><?php echo $char_treffchance . " (" . $char_stats['dex'] . ")"; ?></td>
+        <td><?php echo text_ausgabe("char_glueck", 0, $bg['sprache']); ?></td>
+        <td><?php echo $char_glueck . " (" . $char_stats['luk'] . ")"; ?></td>
+    </tr>
+    <tr>
+        <td colspan="4">
+            &nbsp;
+        </td>
+    </tr>
+
+    <tr>
+        <td><?php echo text_ausgabe("char_stpoints", 0, $bg['sprache']); ?></td>
+        <td><?php echo $char_stpoints; ?></td>
+        <td><?php echo text_ausgabe("char_skpoints", 0, $bg['sprache']); ?></td>
+        <td><?php echo $char_skpoints; ?></td>
+    </tr>
+
+
+    <tr>
+        <td colspan="4">
+            &nbsp;<br>
+            &nbsp;<br>
+            &nbsp;
+        </td>
+    </tr>
+    <tr>
+        <td>Gegenst&auml;nde hergestellt:</td>
+        <td class="text_rechts"><?php echo $dsatz["Items_Crafting"]; ?></td>
+        <td>Rohstoffe abgebaut:</td>
+        <td class="text_rechts"><?php echo $dsatz["Items_Abbau"]; ?></td>
+    </tr>
 </table>
