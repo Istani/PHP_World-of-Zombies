4930299b8f2d2817fb63d83397662eadc0dbab24
Alex Amling <RebornIkarus@gmail.com>
2013-08-01 21:38:02 +0200

    Fehler?
    Versteh nicht warum er <!-- einfach so stehen lässt an paar stellen Ö_ö

--- admin/spielwiese_alex.php
+++ admin/spielwiese_alex.php
@@ -1,4 +1,5 @@
-Alex, bau doch das Menü?!?
+<?php
+/*Alex, bau doch das Menü?!?
 
 TAB: Angreifen
 1. Nahkampf
@@ -21,3 +22,58 @@ Standart auswahl ist immer Angriefen Nahkampf, aber das hat sich ja nach dem ers
 
 Tipp: Viel mit Javascript...
 http://www.php-resource.de/forum/html-javascript-ajax-jquery-und-css/47736-object-nach-tastatureingabe-bewegen.html
+*/
+?>
+
+<script language="JavaScript" type="text/javascript">
+<!--
+function init(){
+	f1      = document.getElementById('f1Div').style;
+	f1.lpos = parseInt(f1.left);
+	f1.tpos = parseInt(f1.top);
+	
+	slide();
+}
+
+function slide()
+{
+	
+	tastatur=window.event.keyCode;
+	
+	if(tastatur == 37)
+    {
+    f1.lpos -= 5;	
+	f1.left  = f1.lpos;
+	}
+	
+	if(tastatur == 39)
+    {
+    f1.lpos += 5;	
+	f1.left  = f1.lpos;
+	}
+	
+	if(tastatur == 38)
+    {
+    f1.tpos += 5;	
+	f1.top  = f1.tpos;
+	}
+	
+	if(tastatur == 40)
+    {
+    f1.tpos -= 5;	
+	f1.top  = f1.tpos;
+	}	
+
+}
+
+//-->
+</script>
+<script for="document" event="onkeydown()" language="JScript" type="text/jscript">
+<!--
+ {
+  init();
+ }
+//-->
+</script>
+
+     <div id="f1Div" style="position: absolute; left: 50px; top: 200px; width: 30px;"><img border="0" name="figur" src="../picture/ks/menu/figur.png" width="180" height="180"></div></a>
