e519979e0560f9662cf1becb744bcba50b679853
Sascha Kaufmann <Sascha.u.kaufmann@googlemail.com>
2013-10-13 13:08:15 +0200

    Export umgebschrieben für Komplette MYSQL Daten für Alex

--- script_mysql_struktur.php
+++ script_mysql_struktur.php
@@ -1,14 +1,13 @@
 <?php
-
-    // PFADE m�ssen angepasst werden!
-    include("_mysql.php");
-    //system("D:\\xampp\\mysql\\bin\\mysqldump.exe -u ".$mysql['user']." -p".$mysql['pw']." --no-data -h ".$mysql['host']." ".$mysql['db']." > D:\\xampp\\htdocs\\zodgame\\TEMP_MYSQL_STRUKTUR.sql", $fp);
-    //system("D:\\xampp\\mysql\\bin\\mysqldump.exe -u ".$mysql['user']." --no-data -h ".$mysql['host']." ".$mysql['db']." > D:\\xampp\\htdocs\\zodgame\\TEMP_MYSQL_STRUKTUR.sql", $fp);
-    system("G:\\xampp\\mysql\\bin\\mysqldump.exe -u " . $mysql['user'] . " --no-data -h " . $mysql['host'] . " " . $mysql['db'] . " > G:\\xampp\\htdocs\\zodgame\\TEMP_MYSQL_STRUKTUR.sql", $fp);
-    //system("mysqldump -u ".$mysql['user']." --no-data -h ".$mysql['host']." ".$mysql['db']." > TEMP_MYSQL_STRUKTUR.sql", $fp);
-    if ($fp == 0)
-	echo "Daten exportiert";
-    else
-	echo "Es ist ein Fehler aufgetreten";
+  // PFADE m�ssen angepasst werden!
+  include("_mysql.php");
+  system("C:\\xampp\\mysql\\bin\\mysqldump.exe -u ".$mysql['user']." -p".$mysql['pw']." -h ".$mysql['host']." ".$mysql['db']." > C:\\xampp\\htdocs\\Wolrd-of-Zombies\\TEMP_MYSQL_STRUKTUR.sql", $fp);
+  //system("D:\\xampp\\mysql\\bin\\mysqldump.exe -u ".$mysql['user']." --no-data -h ".$mysql['host']." ".$mysql['db']." > D:\\xampp\\htdocs\\zodgame\\TEMP_MYSQL_STRUKTUR.sql", $fp);
+  //system("G:\\xampp\\mysql\\bin\\mysqldump.exe -u " . $mysql['user'] . " --no-data -h " . $mysql['host'] . " " . $mysql['db'] . " > G:\\xampp\\htdocs\\zodgame\\TEMP_MYSQL_STRUKTUR.sql", $fp);
+  //system("mysqldump -u ".$mysql['user']." --no-data -h ".$mysql['host']." ".$mysql['db']." > TEMP_MYSQL_STRUKTUR.sql", $fp);
+  if ($fp==0)
+    echo "Daten exportiert";
+  else
+    echo "Es ist ein Fehler aufgetreten";
 ?>
 
